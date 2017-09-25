4cf6a13a8294feff9ec61fde8ebb345d33861eb9
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2017-09-25 00:32:35 -0400

    initial commit to get the ball rollimg

---
+++ calculator.js
@@ -0,0 +1,78 @@
+let readline = require("readline-sync");
+let evaluationStack = [], postfixQueue = [];
+let operators = ['+', '-', '*', '/', '(', ')'];
+
+let initCalculator = function () {
+    getUserInput();
+};
+
+let getUserInput = function () {
+    var inputExpression = readline.question("Please enter a valid infix expression: ");
+    if (inputExpression === "quit") {
+        return;
+    } else {
+        // convert to postfix
+        let infixExpression = inputExpression.replace(/\s+/g,' ').trim(); // get rid of extra whitespaces, ref: https://stackoverflow.com/a/16974697/1705383
+        if (createValidExpression(infixExpression)) {
+            console.log("Postfix: ", postfixQueue);
+            console.log("Value: ", evaluatePostfixExpression());
+        }
+    }
+};
+
+let createValidExpression = function (expression) {
+    let number = '';
+    for (let i = 0; i < expression.length; i++) {
+        if (!isNaN(expression[i]) || expression[i] === " " || operators.indexOf(expression[i]) > -1) {
+            number = (expression[i] === " " || operators.indexOf(expression[i]) > -1)  ? number : number + expression[i];
+            if (expression[i] === " ") {
+                number.length > 0 ? postfixQueue.push(number) : undefined;
+                number = '';
+            } else if (operators.indexOf(expression[i]) > -1) {
+                number.length > 0 ? postfixQueue.push(number) : undefined;
+                postfixQueue.push(expression[i]);
+                number = '';
+            }
+        } else {
+            console.log("Please enter a valid expression");
+            return false;
+        }
+    }
+    return true;
+};
+
+let evaluatePostfixExpression = function () {
+    let token;
+    while (postfixQueue.length > 0) {
+        token = postfixQueue.shift();
+        console.log("postQ: ", postfixQueue);
+
+        if (!isNaN(token)) { // token is a number (operand)
+            evaluationStack.unshift(token);
+            console.log("eval: ", evaluationStack);
+        } else { // token is an operator
+            let firstNumber = evaluationStack.shift();
+            let secondNumber = evaluationStack.shift();
+            let answer = 0;
+
+            switch (token) {
+                case '+': answer = +secondNumber + +firstNumber; // type casting to number
+                break;
+                case '-': answer = +secondNumber - +firstNumber; // type casting to number
+                break;
+                case '*': answer = secondNumber * firstNumber;
+                break;
+                case '/': answer = secondNumber / firstNumber;
+                break;
+            }
+
+            evaluationStack.unshift(answer);
+            console.log("eval: ", evaluationStack);            
+        }
+    }
+
+    return evaluationStack[0];
+};
+
+// used to bootstrap the app
+initCalculator();
